1a1e34657ed388a6a4fa90e552975e42999cd7d9
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-11-01 22:29:20 +0100

    :star: Starting with architect scene

---
+++ Assets/scripts/UIPointLock.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPointLock : MonoBehaviour {
+
+	[SerializeField]
+	Image centerCursor;
+
+	[SerializeField]
+	KeyCode deactivateKey;
+
+	CursorLockMode releaseMode;
+	bool releaseVisibility;
+
+	void OnEnable() {
+		releaseMode = Cursor.lockState;
+		Cursor.lockState = CursorLockMode.Locked;
+		releaseVisibility = Cursor.visible;
+		Cursor.visible = false;
+	}
+
+	void OnDisable() {		
+		Cursor.lockState = releaseMode;
+		Cursor.visible = releaseVisibility;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(deactivateKey)) { 	
+			this.enabled = false;
+		}
+	}
+}
